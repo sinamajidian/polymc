eab47271cc203a55479de8b1bfe5d3715c2ec543
s_majidian <s_majidian@yahoo.com>
2018-12-25 14:06:38 +0100

    checking two functions

--- compare.py
+++ compare.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
 """
-This code is for comparing the estimated haplotype blocks with a grand truth.
+This code is for comparing the estimated diploid haplotype blocks with a grand truth.
 
-usage: python3 compare.py head_truth_21 out_algorithm.txt
+usage: python3  compare.py  adress_files truth estimated_haplotype [vcffile]
+For comparing based on  variation position, assign the  vcffile as the fourth
 
-My first code in python.
-
-sina majdiain
-10 Dec
+Sina Majdiain
+Start: 10 Dec
 
 """
 from sys import argv
@@ -17,76 +16,113 @@ from collections import Counter
 
 
 def parse_hap_truth(file_name):
-	""" extracting haplotype from haplotype truth file  in probhap website
+	""" extracting haplotype from haplotype truth file just heterozygous variants.
 	input: file name
-	output: a dictionary, label:variantpostion , value: allele 0 or 1
+	output: a dictionary, key: position of variant  value: one allele (0 or 1)
+
+	Since it is heterozygous, one allel is enough for comapring.
+	The input haplotype if contain   .vcf is considered as this option
+	a vcf file inwhich GT id contain | . This used in HapCHAT paper and also GIAB project. Also mentioned in part 1.4.2  of
+	"The Variant Call Format (VCF) Version 4.1 Specification"
+
+	20 14370 rs6054257 G A 29 PASS NS=3;DP=14;AF=0.5;DB;H2 GT:GQ:DP:HQ 0|0:48:1:51,51
+	20 17330 . T A 3 q10 NS=3;DP=11;AF=0.017 GT:GQ:DP:HQ 0|0:49:3:58,50
+
+	else it is considered  as a Kuleshov (probhap) format for grand truth:
+
+	2	695745	1	0
+	3	766409	0	1
+	4	801628	0	1
 	"""
 	truth_file = open(file_name, "r")
-	dic = {}
+	dic_hap_truth = {}
 	if '.vcf' in file_name:
 		for line in truth_file:
-			if not line.startswith('#'):
-				line_slice = line.strip().split("\t")
-				GT = line_slice[9]
-				if '|' in GT:
-					variantpostion = int(line_slice[1])
-					dic[variantpostion] = int(GT[0])
+			if not line.startswith('#'):  # GT:GQ:DP:HQ 0|0:48:1:51,51
+				slices_line = line.strip().split("\t")
+				gt_value = slices_line[9]
+				if '|' in gt_value:  # just keep phased variants. some variant may not phased.
+					postion_variant = int(slices_line[1])
+					dic_hap_truth[postion_variant] = int(gt_value[0])
 	else:
 		for line in truth_file:  # 2	695745	1	0
-			line_slice = line.strip().split("\t")
-			label = int(line_slice[0])
-			dic[label] = int(line_slice[3])
-	return dic
+			slices_line = line.strip().split("\t")
+			idx_variant = int(slices_line[0])   # for kuleshov data, I don't know the position of all variants,
+			# I used the indx instead of postion of variant.
+			dic_hap_truth[idx_variant] = int(slices_line[2])
+			# postion_variant = int(slices_line[1])  # if we know all positon we can use this,
+			# dic_hap_truth[postion_variant] = int(slices_line[2])
+	return dic_hap_truth
 
 
-def parse_vcf_freeb(file):
-	""" extracting haplotype from haplotype truth file  in probhap website
-	input: file name
-	output: a dictionary, label:variantpostion , value: allele 0 or 1
+def parse_position_vcf(file):
+	""" extracting position vcf file
+
+	input: vcf file name
+	output: a list of  variant postion
 	"""
 	vcf_file = open(file, "r")
-	list_position = []
+	list_postion_variant = []
 	for line in vcf_file:
-		if not line.startswith('#'):
-			line_slice = line.strip().split("\t")
-			list_position.append(int(line_slice[1]))
-	return list_position
+		if not line.startswith('#'):  # 22	17056280	.	C	A	.	.	AC=2;AF=1.00;AN=2
+			slices_line = line.strip().split("\t")
+			postion_variant = int(slices_line[1])
+			list_postion_variant.append(postion_variant)
+	return list_postion_variant
 
 
 def parse_hap_estimated_blocks(file, list_position):
 
-	""" extracting estimated haplotype blocks from  algorithm's output
+	""" extracting estimated haplotype blocks from  algorithm's output file
+
 	input: file name
 	output: dictionary, label: block number,  value: a dictionary of idx and hap
+
+	the inptut can be like this
+	BLOCK	2	7
+	2	0
+	3	1
+
+	or
+
+	BLOCK: offset: 4 len: 14 phased: 14 SPAN: 25449 fragments 14
+	4	0	1	chr1	801628	C	T	0/1:69:28	0	.	31.00
+	5	0	1	chr1	805678	A	T	0/1:21:70	0	.	100.00
 	"""
-	hap_block_file = open(file, "r")
-	dic = {}
-	dic_in = {}
+	hap_blocks = open(file, "r")
+	dic_blocks = {}  # dic_blocks = {1: dic_block1, 2: dic_block2}
+	dic_block = {}  # dic_block = {2:0 , 3:1 , 28:1}
 	block_idx = 0
 	start = True
-	for line in hap_block_file:
+	for line in hap_blocks:
 			line = line.strip()
 			if line.startswith('B'):  # new block started
-				block_idx += 1
 				if not start:
-					dic[block_idx] = dic_in
+					block_idx += 1
+					dic_blocks[block_idx] = dic_block
 				start = False
-				dic_in = {}
-			elif not line.startswith('*'):  # & ('-' not in line)
-				line_slice = line.split("\t")  # 4	0	1	chr1	801628	C ...#hapcut2
-				if '-' not in line_slice[1]:
-					allele = int(line_slice[1])
-					idx = int(line_slice[0])
-					if len(list_position):
+				dic_block = {}
+			elif not line.startswith('*'):
+				slices_line = line.split("\t")
+				if '-' not in slices_line[1]:
+					allele = int(slices_line[1])
+					idx = int(slices_line[0])
+					if not len(list_position):  # for comparing based on index, not variation position
+						dic_block[idx] = allele
+					else:  # for comparing based on variation position
 						var_position = list_position[idx - 1]
-					else:
-						var_position = idx  # for the current matlab output the indx is 1 or 2
-						# var_position = int(line_slice[4])  # hapcut
+						dic_block[var_position] = allele
+						# For future  if len(slices_line) > 3:  # hapcut2: 4	0	1	chr1	801628	C ...
+						# var_position = int(line_slice[4])
+	block_idx += 1
+	dic_blocks[block_idx] = dic_block
+	return dic_blocks
 
-					dic_in[var_position] = allele
-	dic[block_idx] = dic_in
-	return dic
 
+
+
+
+#  checked to here
 
 def compare_dics(dic_truth, dic_block_estimated):
 	""" comaring estimated block of haplotype with truth haplotype
@@ -194,13 +230,14 @@ if __name__ == "__main__":
 	address = argv[1]
 	filename_hap_truth = address+argv[2]
 	filename_hap_matlab = address+argv[3]
-	if len(argv) > 4:
+	if len(argv) > 4: # for comparing based on  variation position, assign the  filename_vcf # i know that hapcut may contain
 		filename_vcf = address+argv[4]
-		list_position = parse_vcf_freeb(filename_vcf)
+		list_position = parse_position_vcf(filename_vcf)
 	else:
 		list_position = []
 
 	dic_hap_truth = parse_hap_truth(filename_hap_truth)
+	a = dic_hap_truth[51174662]
 	dic_hap_estimated = parse_hap_estimated_blocks(filename_hap_matlab, list_position)
 
 
